6cbeebd6ae9ef7387fa308275a3dcc439199841d
Ryan McGowan <vitomaserati@gmail.com>
2022-11-16 18:57:13 -0800

    role bases access

--- frontend/src/users/SignUpForm.js
+++ frontend/src/users/SignUpForm.js
@@ -68,19 +68,17 @@ function SignUpForm() {
 						/>
 					</div>
 					<div className="col-sm-6 form-group">
-        <label htmlFor="password">Password</label>
-        <input
-            type="password"
-            required
-            value={user.password}
-            onChange={e => setUser({ ...user, password: e.target.value })}
-            className="form-control"
-            id="password"
-            name="password"
-        />
-    </div>
-
-  
+						<label htmlFor="password">Password</label>
+						<input
+							type="password"
+							required
+							value={user.password}
+							onChange={e => setUser({ ...user, password: e.target.value })}
+							className="form-control"
+							id="password"
+							name="password"
+						/>
+					</div>
 				</div>
 				<input className="btn btn-primary" type="submit" value="Sign Up" />
 			</form>
@@ -89,8 +87,3 @@ function SignUpForm() {
 }
 
 export default SignUpForm
-
-
-
-
-  
